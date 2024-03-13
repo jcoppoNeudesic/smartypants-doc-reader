94824e0d596b02d101cf536b5292aa1e72938922
Jayson Coppo <jayson.coppo@neudesic.com>
2024-03-13 13:49:42 -0700

    random launch stuff?

--- obj/Debug/net8.0/SmartypantsDocReader.AssemblyInfo.cs
+++ obj/Debug/net8.0/SmartypantsDocReader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartypantsDocReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590ac245ed718e71a406b691074d46883d2968a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartypantsDocReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartypantsDocReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
